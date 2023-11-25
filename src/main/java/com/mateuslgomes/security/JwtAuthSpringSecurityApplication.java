2e24d3a9896212871482fa86d6d50933341e3831
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-11-25 20:42:14 -0300

    Criando projetos

---
+++ src/main/java/com/mateuslgomes/security/JwtAuthSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mateuslgomes.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuslgomes/security/JwtAuthSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuslgomes.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
