780f4155b23666153b39cf1d1fdaf3581f5d286a
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-11-28 22:59:59 -0300

    criando tabela "_user_"

---
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE _user_ (
+    id UUID DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    password VARCHAR(254) NOT NULL,
+    PRIMARY KEY (id)
+);
