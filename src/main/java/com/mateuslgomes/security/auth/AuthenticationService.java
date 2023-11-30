89a56318185a079382d71697d8c87ce976e02771
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-11-30 17:12:30 -0300

    Implementando authenticate no AuthenticationService

--- src/main/java/com/mateuslgomes/security/auth/AuthenticationService.java
+++ src/main/java/com/mateuslgomes/security/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.mateuslgomes.security.user.Role;
 import com.mateuslgomes.security.user.User;
 import com.mateuslgomes.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -31,7 +34,17 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.email())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
     }
 
 }
