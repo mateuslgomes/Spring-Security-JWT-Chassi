e028bc448e6afcf13da049f20ff8cd41a1258db3
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-11-30 19:22:00 -0300

    Adicionando UNIQUE no campo email

--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -4,7 +4,7 @@ CREATE TABLE _user_ (
     id UUID DEFAULT uuid_generate_v4(),
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(254) NOT NULL,
+    email VARCHAR(254) UNIQUE NOT NULL,
     password VARCHAR(254) NOT NULL,
     PRIMARY KEY (id)
 );
