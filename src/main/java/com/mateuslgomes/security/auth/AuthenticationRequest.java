4e307f6e4a068b3e6038575ad6a5e52e1ffb9f50
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-12-02 20:45:04 -0300

    Add validações para cadastro e login de usuários

--- src/main/java/com/mateuslgomes/security/auth/AuthenticationRequest.java
+++ src/main/java/com/mateuslgomes/security/auth/AuthenticationRequest.java
@@ -1,11 +1,16 @@
 package com.mateuslgomes.security.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record AuthenticationRequest(
 
+    @Email
     String email,
+
+    @Size(min= 8, max = 254)
     String password
 
 ) {}
--- src/main/java/com/mateuslgomes/security/auth/RegisterRequest.java
+++ src/main/java/com/mateuslgomes/security/auth/RegisterRequest.java
@@ -1,13 +1,23 @@
 package com.mateuslgomes.security.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record RegisterRequest(
 
+    @Size(min = 2, max = 100)
     String firstName,
+
+    @Size(min = 2, max = 100)
     String lastName,
+
+    @Email
+    @Size(max = 254)
     String email,
+
+    @Size(min= 8, max = 254)
     String password
 
 ) {}
