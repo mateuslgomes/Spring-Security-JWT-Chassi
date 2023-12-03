8f8b8e2256f5f5e63ce4f7eccf8633feef5b5e4b
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-12-02 21:57:59 -0300

    Add ExceptionHandler de ValidationException

--- src/main/java/com/mateuslgomes/security/exceptions/handler/CustomExceptionHandler.java
+++ src/main/java/com/mateuslgomes/security/exceptions/handler/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.mateuslgomes.security.exceptions.handler;
 
 import com.mateuslgomes.security.exceptions.UserAlreadyExistsException;
+import com.mateuslgomes.security.exceptions.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,11 @@ public class CustomExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<String> UserAlreadyExistsException(UserAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> ValidationException(ValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 }
