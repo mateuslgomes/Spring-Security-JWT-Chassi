b03e3ecda6cb18f8aea553f25e9330776271f84c
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-12-05 18:37:52 -0300

    feat: Criando entidade token e realizando relacionamento com a tabela User

---
+++ src/main/java/com/mateuslgomes/security/token/TokenRepository.java
@@ -0,0 +1,5 @@
+package com.mateuslgomes.security.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {}
---
+++ src/main/java/com/mateuslgomes/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.mateuslgomes.security.token;
+
+public enum TokenType {
+    BEARER
+}
