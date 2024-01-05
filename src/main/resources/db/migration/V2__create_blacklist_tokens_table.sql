a68154930d3dafb20ef6f3cd80488b0966c3a365
developer <mateuslgomes.dev@gmail.com>
2024-01-05 12:34:18 -0300

    feat: remove entity blacklist

--- src/main/resources/db/migration/V2__create_blacklist_tokens_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE blacklist_tokens (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_token VARCHAR(255) UNIQUE,
-    revocation_date TIMESTAMP
-);
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
